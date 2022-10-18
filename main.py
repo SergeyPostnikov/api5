4209dbee41c2ff2929a4bf57d3a6b9404bcbdf43
Sergey Postnikov <ser.post2017@gmail.com>
2022-10-18 16:46:03 +0300

    get_statistic exdended

--- main.py
+++ main.py
@@ -14,28 +14,6 @@ def get_vacancies(text: str) -> dict:
     return response.json()
 
 
-def get_statistic() -> dict:
-    langs = ['python', 'Java', 'Javascript', 'PHP']
-    statistic = {}
-    for lang in langs:
-        statistic[lang] = get_vacancies(text=lang)["found"]
-    return statistic
-
-
-def get_python_salary() -> list:
-    vacancies = []
-    for vacancy in get_vacancies('python').get('items'):
-        if vacancy['salary'] is not None:
-            vacancies.append(
-                {'from': vacancy['salary'].get('from'),
-                'to': vacancy['salary'].get('to'), 
-                'currency': vacancy['salary'].get('currency'), 
-                'gross': vacancy['salary'].get('gross')})
-        else:
-            vacancies.append(None)
-    return vacancies
-
-
 def predict_rub_salary(vacancy: dict) -> float:
     salary = vacancy['salary']
     if salary is not None and salary['currency'] == 'RUR':
@@ -48,8 +26,43 @@ def predict_rub_salary(vacancy: dict) -> float:
     return None
 
 
-if __name__ == '__main__':
-    # from pprint import pprint
-    # pprint(get_python_salary())
+def get_statistic() -> dict:
+    langs = ['python', 'Java', 'Javascript', 'PHP']
+    statistic = {}
+    for lang in langs:
+        vacancies = get_vacancies(text=lang)
+        total = 0
+        vacancies_processed = 0
+        for vacancy in vacancies.get('items'):
+            salary = predict_rub_salary(vacancy) 
+            if salary is not None:
+                total += salary
+                vacancies_processed += 1
+        statistic[lang] = {
+            "vacancies_found": vacancies["found"],
+            "vacancies_processed": vacancies_processed,
+            "average_salary": total // vacancies_processed
+        }
+    return statistic
+
+
+def get_python_salary() -> list:
+    vacancies = []
     for vacancy in get_vacancies('python').get('items'):
-        print(predict_rub_salary(vacancy))
+        if vacancy['salary'] is not None:
+            vacancies.append({
+                'from': vacancy['salary'].get('from'),
+                'to': vacancy['salary'].get('to'), 
+                'currency': vacancy['salary'].get('currency'), 
+                'gross': vacancy['salary'].get('gross')})
+        else:
+            vacancies.append(None)
+    return vacancies
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    # pprint(get_python_salary())
+    # for vacancy in get_vacancies('python').get('items'):
+    #     print(predict_rub_salary(vacancy))
+    pprint(get_statistic())
