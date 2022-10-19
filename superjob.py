ee0204bc01e9cd4ffce98de6206d18f41980691e
Sergey Postnikov <ser.post2017@gmail.com>
2022-10-19 23:06:50 +0300

    get_vacancies implemented

--- superjob.py
+++ superjob.py
@@ -6,10 +6,11 @@ from dotenv import load_dotenv
 
 def get_vacancies():
     url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {
-        'X-Api-App-Id': os.getenv('API_SUPERJOB')
-        }
-    response = requests.get(url, headers=headers)
+    headers = {'X-Api-App-Id': os.getenv('API_SUPERJOB')}
+    payload = {
+        't': '4',
+        'keyword': 'python'}
+    response = requests.get(url, params=payload, headers=headers)
     response.raise_for_status()
     return response.json()
 
@@ -19,4 +20,4 @@ if __name__ == '__main__':
     load_dotenv()
     vacancies = get_vacancies()["objects"]
     for vacancy in vacancies:
-        pprint(vacancy.get("profession"))
+        print(vacancy.get("profession"), vacancy.get("town").get("title"))
